badfd73edb1ff9d890c00706edfca7314c45a922
김민제 <mu8786@gmail.com>
2022-12-06 02:58:40 +0900

    add editor

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import dynamic from 'next/dynamic';
+import styles from '../styles/Home.module.css';
+
+const myEditor = () => {
+  const Editor = dynamic(() => import('../components/editor'));
+  return <Editor />;
+};
 
 export default function Home() {
   return (
@@ -15,7 +21,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
+        {myEditor()}
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
@@ -67,5 +73,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
